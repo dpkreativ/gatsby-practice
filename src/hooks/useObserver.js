058ca4a44208e44763b9e0fc192c20572bbc73eb
Divine Orji <dpkreativ@gmail.com>
2022-05-24 16:27:56 +0100

    create useObserver hook

---
+++ src/hooks/useObserver.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+export function useObserver(ref, options) {
+  const { rootMargin } = options;
+
+  const [observerEntry, setObserverEntry] = useState(null);
+
+  useEffect(() => {
+    if (!ref?.current) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setObserverEntry(entry);
+        if (entry.isIntersecting) {
+          observer.unobserve(entry.target);
+        }
+      },
+      { rootMargin }
+    );
+
+    observer.observe(ref.current);
+  }, [ref, rootMargin]);
+
+  return observerEntry;
+}
